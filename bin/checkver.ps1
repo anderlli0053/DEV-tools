17fb7dae1160f89427820a7f6fb704fb227f80d4
Andrew Poženel - SDT <anderlli0053@live.com>
2022-06-28 12:11:18 +0200

    Updated Maintenance scripts

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 5
+
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
